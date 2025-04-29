43f670cf0a15ad591e3ae0d3b520cd559e238f22
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2025-04-29 18:07:47 -0300

    resolvido

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,10 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+# Vamos criar uma função que recebe dois dados e retorna a concatenação deles
+def concatena_dados(dado1, dado2):
+    """
+    Função que recebe dois dados e retorna a concatenação deles
+    :param dado1: Primeiro dado
+    :param dado2: Segundo dado
+    :return: Concatenação dos dados
+    """
+
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,10 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos criar uma função que recebe dois números e retorna a soma deles
+def soma_numeros(num1, num2):
+    """
+    Função que recebe dois números e retorna a soma deles
+    :param num1: Primeiro número
+    :param num2: Segundo número
+    :return: Soma dos números
+    """
+    return num1 + num2
